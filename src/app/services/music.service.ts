19db3a8d31ea8c051f4f7dc49fac06affbe05790
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-08-23 17:18:45 -0300

    Implementação de métodos e dependências

--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Music } from '../models/music';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MusicService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getMusics():Observable<Music[]>{
+    return this.http.get<Music[]>(`${environment.baseUrl}/music`);
+  }
+
+  getMusicById(id:number|string):Observable<Music>{
+    return this.http.get<Music>(`${environment.baseUrl}/music/${id}`);
+  }
 }
