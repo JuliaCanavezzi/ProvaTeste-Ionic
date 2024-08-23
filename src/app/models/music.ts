2a38ace12b5428a50da5198e767a200d637ed291
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-08-23 16:00:30 -0300

    Create interface

---
+++ src/app/models/music.ts
@@ -0,0 +1,10 @@
+export interface Music{
+  id: number,
+  name: string,       //nome
+  nameArtist: string, //nome do artista
+  realeseDate: Date,  //data de lançamento
+  musicalStyle: string,  //estilo musical
+  duration: string, //duração
+  albumName: string, //nome do album
+  composer: string //compositor
+}
